94fd71cdfab02ec05d919ac931297d6857afd1b1
MansourHussain <MansourHussain@outlook.sa>
2020-04-24 01:08:59 +0300

    added some codes

--- main.js
+++ main.js
@@ -1,31 +1,48 @@
 
 let register = document.querySelector(".register");
 
+let citizens_quantity = document.querySelector('.citizens-quantity');
+let citizens_archived = document.querySelector('.citizens-archived');
+let naturalized_quantity = document.querySelector('.naturalized-quantity');
+let naturalized_archived = document.querySelector('.naturalized-archived');
+let births_quantity = document.querySelector('.births-quantity');
+let births_archived = document.querySelector('.births-archived');
+let deaths_quantity = document.querySelector('.deaths-quantity');
+let deaths_archived = document.querySelector('.deaths-archived');
 
-register.addEventListener('click', function () {
-    let newCitizens_files = document.querySelectorAll(".newcitizens-files").value;
-    let archived_newCitizens_files = document.querySelectorAll(".archived-newcitizens-files").value;
-    let newnaturalized_files = document.querySelectorAll(".newnaturalized-files").value;
-    let archived_newNaturalized_files = document.querySelectorAll(".archived-newnaturalized-files").value;
+total_quantity = document.querySelector('.total-quantity');
+total_archived = document.querySelector('.total-archived');
 
 
-    let nat_ctz_qua = document.querySelectorAll('.citizens-quantity');
-    // let nat_ctz_arch = document.querySelectorAll('.citizens-archived');
-    // let non_ctz_qua = document.querySelectorAll('.non-native-ctz-quantity');
-    // let nat_ctz_arch = document.querySelectorAll('.non-native-ctz-archived');
-    // let births_qua = document.querySelectorAll('.births-quantity');
-    // let births_arch = document.querySelectorAll('.births-archived');
-    // let deaths_qua = document.querySelectorAll('.deaths-quantity');
-    // let deaths_qua = document.querySelectorAll('.deaths-archived');
-    // let total_qua = document.querySelectorAll('.total-quantity');
-    // let total_arch = document.querySelectorAll('.total-archived');
+register.addEventListener('click', function() {
 
-    nat_ctz_qua.innerHTML = `${newCitizens_files}`;
+    let newCitizens_files = document.querySelector(".newcitizens-files").value;
+    let archived_newCitizens_files = document.querySelector(".archived-newcitizens-files").value;
+    let newNaturalized_files = document.querySelector(".newnaturalized-files").value;
+    let archived_newNaturalized_files = document.querySelector(".archived-newnaturalized-files").value;
+    let newBirths_files = document.querySelector(".newbirths-files").value;
+    let archived_newBirths_files = document.querySelector(".archived-newbirths-files").value;
+    let newDeaths_files = document.querySelector(".newdeaths-files").value;
+    let archived_newDeaths_files = document.querySelector(".archived-newdeaths-files").value;
 
 
     
-}) 
 
+    citizens_quantity.innerHTML = `${newCitizens_files}`;
+    citizens_archived.innerHTML = `${archived_newCitizens_files}`;
+    naturalized_quantity.innerHTML = `${newNaturalized_files}`;
+    naturalized_archived.innerHTML = `${archived_newNaturalized_files}`;
+    births_quantity.innerHTML = `${newBirths_files}`;
+    births_archived.innerHTML = `${archived_newBirths_files}`;
+    deaths_quantity.innerHTML = `${newDeaths_files}`;
+    deaths_archived.innerHTML = `${archived_newDeaths_files}`;
+
+    var allFiles_quantity = (newCitizens_files+ newNaturalized_files);
+    total_quantity.innerHTML = `${allFiles_quantity}` ;
+    // let allFiles_archived;
+
+    
+}) 
 
 
 
